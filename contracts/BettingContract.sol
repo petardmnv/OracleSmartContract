d04d0b8a0381cc9509cd9be5a6426e7fd82aff1b
petardmnv <petar.dmnv@gmail.com>
2021-10-31 00:42:42 +0300

    [Delete] Migrations.sol and BettingContract.sol login now in Oracle.sol

--- contracts/BettingContract.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
-import {SafeMath} from "./SafeMath.sol";
-contract BettingContract {
-	using SafeMath for uint256;
-
-	constructor() public {
-	}
-}
